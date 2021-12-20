1a9b5509a394bc72f2ffc63df68cb63c32f46caa
suryadheeshjith <Surya.dheeshjith@gmail.com>
2021-12-20 20:30:22 +0530

    Added other DQN nets

--- agent.py
+++ agent.py
@@ -3,7 +3,7 @@ import torch as T
 import torch.nn as nn
 import torch.autograd as autograd
 from utils import ReplayBuffer
-from network import ConvDQN, LinearDQN
+from network import ConvDQN, LinearDQN, DuelingDQN
 
 class Agent():
 	def __init__(self, env, network, learning_rate, gamma, eps_max, eps_min, eps_dec):
@@ -88,8 +88,8 @@ class DQNAgent(Agent):
 
 		self.learn_step_counter = 0
 		self.replace_cnt = replace_cnt
-		self.q_eval = LinearDQN(env.env_shape, env.no_of_actions)
-		self.q_target = LinearDQN(env.env_shape, env.no_of_actions)
+		self.q_eval = ConvDQN(env.env_shape, env.no_of_actions)
+		self.q_target = ConvDQN(env.env_shape, env.no_of_actions)
 
 
 	def get_action(self, state):
@@ -106,21 +106,24 @@ class DQNAgent(Agent):
 		if self.learn_step_counter % self.replace_cnt == 0:
 			self.q_target.load_state_dict(self.q_eval.state_dict())
 
-	def update(self, batch_size):
-
+	def get_batch_tensors(self, batch_size):
 		batch = self.replay_buffer.sample(batch_size)
 		states, actions, rewards, next_states, dones = batch
 		states_t = T.tensor(states, dtype=T.float).to(self.q_eval.device)
 		actions_t = T.tensor(actions).to(self.q_eval.device)
 		rewards_t = T.tensor(rewards, dtype=T.float).to(self.q_eval.device)
 		next_states_t = T.tensor(next_states, dtype=T.float).to(self.q_eval.device)
+		return states_t, actions_t, rewards_t, next_states_t
 
+	def update(self, batch_size):
+
+		states_t, actions_t, rewards_t, next_states_t = self.get_batch_tensors(batch_size)
 		self.q_eval.optimizer.zero_grad()
 
 		self.replace_target_network()
 
 		indices = np.arange(batch_size)
-		curr_Q = self.q_eval.forward(states_t)[indices, actions]
+		curr_Q = self.q_eval.forward(states_t)[indices, actions_t]
 		max_next_Q = self.q_target.forward(next_states_t).max(1)[0]
 		expected_Q = rewards_t + self.gamma * max_next_Q
 
@@ -139,7 +142,35 @@ class DQNAgent(Agent):
 			self.update(batch_size)
 
 
-class DoubleDQNAgent(Agent):
+class DoubleDQNAgent(DQNAgent):
+	def __init__(self, env, network, learning_rate, gamma, eps_max, eps_min, eps_dec, buffer_size, replace_cnt):
+		super().__init__(env, network, learning_rate, gamma, eps_max, eps_min, eps_dec, buffer_size, replace_cnt)
+
+
+	def update(self, batch_size):
+
+		states_t, actions_t, rewards_t, next_states_t = self.get_batch_tensors(batch_size)
+
+		self.q_eval.optimizer.zero_grad()
+
+		self.replace_target_network()
+
+		indices = np.arange(batch_size)
+		curr_Q = self.q_eval.forward(states_t)[indices, actions_t]
+		max_indices = self.q_eval.forward(next_states_t).max(1)[1]
+		max_next_Q = self.q_target.forward(next_states_t)[indices, max_indices]
+		expected_Q = rewards_t + self.gamma * max_next_Q
+
+		loss = self.q_eval.MSE_loss(curr_Q, expected_Q).to(self.q_eval.device)
+
+		loss.backward()
+		self.q_eval.optimizer.step()
+		self.learn_step_counter += 1
+
+		self.dec_eps()
+
+
+class DuelingDQNAgent(Agent):
 	def __init__(self, env, network, learning_rate, gamma, eps_max, eps_min, eps_dec, buffer_size, replace_cnt):
 		super().__init__(env, network, learning_rate, gamma, eps_max, eps_min, eps_dec)
 
@@ -147,8 +178,8 @@ class DoubleDQNAgent(Agent):
 
 		self.learn_step_counter = 0
 		self.replace_cnt = replace_cnt
-		self.q_eval = LinearDQN(env.env_shape, env.no_of_actions)
-		self.q_target = LinearDQN(env.env_shape, env.no_of_actions)
+		self.q_eval = DuelingDQN(env.env_shape, env.no_of_actions)
+		self.q_target = DuelingDQN(env.env_shape, env.no_of_actions)
 
 
 	def get_action(self, state):
@@ -158,30 +189,36 @@ class DoubleDQNAgent(Agent):
 
 		else:
 			state = T.tensor(state, dtype=T.float).unsqueeze(0).to(self.q_eval.device)
-			actions = self.q_eval.forward(state)
-			return T.argmax(actions).item()
+			_, advantage = self.q_eval.forward(state)
+			return T.argmax(advantage).item()
 
 	def replace_target_network(self):
 		if self.learn_step_counter % self.replace_cnt == 0:
 			self.q_target.load_state_dict(self.q_eval.state_dict())
 
-	def update(self, batch_size):
-
+	def get_batch_tensors(self, batch_size):
 		batch = self.replay_buffer.sample(batch_size)
 		states, actions, rewards, next_states, dones = batch
 		states_t = T.tensor(states, dtype=T.float).to(self.q_eval.device)
 		actions_t = T.tensor(actions).to(self.q_eval.device)
 		rewards_t = T.tensor(rewards, dtype=T.float).to(self.q_eval.device)
 		next_states_t = T.tensor(next_states, dtype=T.float).to(self.q_eval.device)
+		return states_t, actions_t, rewards_t, next_states_t
 
+	def update(self, batch_size):
+
+		states_t, actions_t, rewards_t, next_states_t = self.get_batch_tensors(batch_size)
 		self.q_eval.optimizer.zero_grad()
 
 		self.replace_target_network()
 
 		indices = np.arange(batch_size)
-		curr_Q = self.q_eval.forward(states_t)[indices, actions]
-		max_indices = self.q_eval.forward(next_states_t).max(1)[1]
-		max_next_Q = self.q_target.forward(next_states_t)[indices, max_indices]
+		Vs, As = self.q_eval.forward(states_t)
+		curr_Q = T.add(Vs, (As - As.mean(dim=1, keepdim=True)))[indices, actions_t]
+
+		Vns, Ans = self.q_target.forward(next_states_t)
+		max_next_Q = T.add(Vs, (As - As.mean(dim=1, keepdim=True))).max(1)[0]
+
 		expected_Q = rewards_t + self.gamma * max_next_Q
 
 		loss = self.q_eval.MSE_loss(curr_Q, expected_Q).to(self.q_eval.device)
@@ -199,13 +236,52 @@ class DoubleDQNAgent(Agent):
 			self.update(batch_size)
 
 
+class DuelingDoubleDQNAgent(DuelingDQNAgent):
+	def __init__(self, env, network, learning_rate, gamma, eps_max, eps_min, eps_dec, buffer_size, replace_cnt):
+		super().__init__(env, network, learning_rate, gamma, eps_max, eps_min, eps_dec, buffer_size, replace_cnt)
+
+
+	def update(self, batch_size):
+
+		states_t, actions_t, rewards_t, next_states_t = self.get_batch_tensors(batch_size)
+		self.q_eval.optimizer.zero_grad()
+
+		self.replace_target_network()
+
+		indices = np.arange(batch_size)
+		Vs, As = self.q_eval.forward(states_t)
+		curr_Q = T.add(Vs, (As - As.mean(dim=1, keepdim=True)))[indices, actions_t]
+
+		Vs_, As_ = self.q_eval.forward(next_states_t)
+		max_indices = T.add(Vs_, (As_ - As_.mean(dim=1, keepdim=True))).max(1)[1]
+		Vns, Ans = self.q_target.forward(next_states_t)
+		max_next_Q = T.add(Vns, (Ans - Ans.mean(dim=1, keepdim=True)))[indices, max_indices]
+
+		expected_Q = rewards_t + self.gamma * max_next_Q
+
+		loss = self.q_eval.MSE_loss(curr_Q, expected_Q).to(self.q_eval.device)
+
+		loss.backward()
+		self.q_eval.optimizer.step()
+		self.learn_step_counter += 1
+
+		self.dec_eps()
 
 def get_agent(env, args):
 	if args.network in ["LinearDQN", "SimpleConvDQN"]:
-		return Simple_Agent(env, args.network, args.learning_rate, args.gamma, args.eps_max, args.eps_min, args.eps_dec, args.max_buffer_size)
+		return Simple_DQNAgent(env, args.network, args.learning_rate, args.gamma, args.eps_max, args.eps_min, args.eps_dec, args.max_buffer_size)
 
 	elif args.network == "ConvDQN":
 		return DQNAgent(env, args.network, args.learning_rate, args.gamma, args.eps_max, args.eps_min, args.eps_dec, args.max_buffer_size, args.update_steps)
 
 	elif args.network == "DoubleDQN":
 		return DoubleDQNAgent(env, args.network, args.learning_rate, args.gamma, args.eps_max, args.eps_min, args.eps_dec, args.max_buffer_size, args.update_steps)
+
+	elif args.network == "DuelingDQN":
+		return DuelingDQNAgent(env, args.network, args.learning_rate, args.gamma, args.eps_max, args.eps_min, args.eps_dec, args.max_buffer_size, args.update_steps)
+
+	elif args.network == "DuelingDoubleDQN":
+		return DuelingDoubleDQNAgent(env, args.network, args.learning_rate, args.gamma, args.eps_max, args.eps_min, args.eps_dec, args.max_buffer_size, args.update_steps)
+
+	else:
+		raise "Enter Valid Network! Choose from : LinearDQN, SimpleConvDQN, ConvDQN, DoubleDQN, DuelingDQN, DuelingDoubleDQN"
--- network.py
+++ network.py
@@ -3,6 +3,33 @@ import torch.nn as nn
 import torch.optim as optim
 import numpy as np
 
+class LinearDQN(nn.Module):
+
+	def __init__(self, input_dim, output_dim):
+		super().__init__()
+		self.input_dim = input_dim
+		self.output_dim = output_dim
+
+		self.fc = nn.Sequential(
+			nn.Linear(self.input_dim[1]*self.input_dim[2], 256),
+			nn.ReLU(),
+			nn.Linear(256, 512),
+			nn.ReLU(),
+			nn.Linear(512, 256),
+			nn.ReLU(),
+			nn.Linear(256, self.output_dim)
+		)
+
+		self.optimizer = optim.Adam(self.parameters())
+		self.MSE_loss = nn.MSELoss()
+		self.device = T.device('cuda:0' if T.cuda.is_available() else 'cpu')
+		self.to(self.device)
+
+	def forward(self, state):
+		qvals = self.fc(T.flatten(state,start_dim=1))
+		return qvals
+
+
 class ConvDQN(nn.Module):
 
 	def __init__(self, input_dim, output_dim):
@@ -45,28 +72,45 @@ class ConvDQN(nn.Module):
 		return qvals
 
 
-class LinearDQN(nn.Module):
+class DuelingDQN(nn.Module):
 
 	def __init__(self, input_dim, output_dim):
 		super().__init__()
 		self.input_dim = input_dim
 		self.output_dim = output_dim
 
-		self.fc = nn.Sequential(
-			nn.Linear(self.input_dim[1]*self.input_dim[2], 256),
+		self.conv = nn.Sequential(
+			nn.Conv2d(1, 32, kernel_size=4, stride=2),
 			nn.ReLU(),
-			nn.Linear(256, 512),
-			nn.ReLU(),
-			nn.Linear(512, 256),
-			nn.ReLU(),
-			nn.Linear(256, self.output_dim)
+			#nn.Conv2d(32, 64, kernel_size=4, stride=2),
+			#nn.ReLU(),
+			nn.Conv2d(32, 16, kernel_size=3, stride=1),
+			nn.ReLU()
 		)
+
+		self.fc_input_dim = self.feature_size()
+		self.fc1 = nn.Sequential(
+			nn.Linear(self.fc_input_dim, 64),
+			nn.ReLU()
+		)
+		self.V = nn.Linear(64, 1)
+		self.A = nn.Linear(64, self.output_dim)
 
 		self.optimizer = optim.Adam(self.parameters())
 		self.MSE_loss = nn.MSELoss()
 		self.device = T.device('cuda:0' if T.cuda.is_available() else 'cpu')
 		self.to(self.device)
 
+	def feature_size(self):
+		state = T.zeros(1, *self.input_dim)
+		dims = self.conv(state)
+		return int(np.prod(dims.size()))
+
 	def forward(self, state):
-		qvals = self.fc(T.flatten(state,start_dim=1))
-		return qvals
+		features = self.conv(state)
+		features = features.view(features.size(0), -1)
+		outs = self.fc1(features)
+		V = self.V(outs)
+		A = self.A(outs)
+
+		return V, A
